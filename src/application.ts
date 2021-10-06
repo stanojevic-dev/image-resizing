2110878602c2028b3df3ad94a45c456229d71a9b
stanojevic-dev <stanojevic.development@gmail.com>
2021-10-06 09:06:53 +0200

    setup loopback application

---
+++ src/application.ts
@@ -0,0 +1,64 @@
+import {BootMixin} from '@loopback/boot';
+import {ApplicationConfig} from '@loopback/core';
+import {RepositoryMixin} from '@loopback/repository';
+import {RestApplication} from '@loopback/rest';
+import {
+  RestExplorerBindings,
+  RestExplorerComponent,
+} from '@loopback/rest-explorer';
+import {ServiceMixin} from '@loopback/service-proxy';
+import path from 'path';
+import {MySequence} from './sequence';
+
+export {ApplicationConfig};
+
+export class ImageResizingApplication extends BootMixin(
+  ServiceMixin(RepositoryMixin(RestApplication)),
+) {
+  constructor(options: ApplicationConfig = {}) {
+    super(options);
+
+    this.setUpBindings();
+
+    // Set up the custom sequence
+    this.sequence(MySequence);
+
+    // Set up default home page
+    this.static('/', path.join(__dirname, '../public'));
+
+    // Customize @loopback/rest-explorer configuration here
+    this.configure(RestExplorerBindings.COMPONENT).to({
+      path: '/explorer',
+    });
+    this.component(RestExplorerComponent);
+
+    this.projectRoot = __dirname;
+    // Customize @loopback/boot Booter Conventions here
+    this.bootOptions = {
+      controllers: {
+        // Customize ControllerBooter Conventions here
+        dirs: ['controllers'],
+        extensions: ['.controller.js'],
+        nested: true,
+      },
+    };
+  }
+
+  setUpBindings(): void {
+    console.log('Setting up bindings...');
+  }
+
+  async boot() {
+    console.log('Booting...');
+    return super.boot();
+  }
+
+  // Unfortunately, TypeScript does not allow overriding methods inherited
+  // from mapped types. https://github.com/microsoft/TypeScript/issues/38496
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  async start() {
+    console.log('Starting...');
+    return super.start();
+  }
+}
